b267fde91dc032436c273dfacb86151857ef6cf3
Thanh Nhã <23709251.nha@student.iuh.edu.vn>
2025-05-06 23:25:26 +0700

    Update main.py

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,10 +1,31 @@
-import boto3
+import requests
+import gzip
+from io import BytesIO
 
+def get_file(base, key):
+    url = base + key
+    resp = requests.get(url, stream=True)
+    resp.raise_for_status()
+    return resp
 
 def main():
-    # your code here
-    pass
+    s3_base = "https://data.commoncrawl.org/"
+    key = "crawl-data/CC-MAIN-2022-05/wet.paths.gz"
+    
+    # Tải file .gz từ S3
+    resp_wet = get_file(s3_base, key)
+    
+    # Giải nén dữ liệu .gz và lấy URI từ dòng đầu tiên
+    with gzip.open(BytesIO(resp_wet.content), 'rt', encoding='utf-8') as f:
+        uri_path = f.readline().strip()
 
+    # Tải file WARC từ URI lấy được
+    resp_warc = get_file(s3_base, uri_path)
+    
+    # Lặp qua các dòng trong file WARC và giải mã chuỗi byte thành chuỗi văn bản
+    for line in resp_warc.iter_lines(decode_unicode=True):
+        if line:
+            print(line)
 
 if __name__ == "__main__":
     main()
